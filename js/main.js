fe46042337fc83c880dd8e98c0eee7d08e7b20af
Alexandr <80934396+Vaso1911@users.noreply.github.com>
2023-08-18 18:24:25 +0300

    улучшил код

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const num = 266219;
-const numSplit = String(num).split('').map(Number);
-const numSplitMult = numSplit.reduce( (acc, value) => acc * value, 1);
+const numSplit = String(num).split('').map(Number).reduce( (acc, value) => acc * value, 1);
 
-console.log(` ${ numSplitMult }`);
 
-let numDegree = numSplitMult
+console.log(` ${numSplit}`);
+
+let numDegree = numSplit
 for (let i = 1; i < 3; i++) {
-  numDegree *= numSplitMult; // надеюсь я правильно понял про оператор и использовал только 1 (*)
+  numDegree *= numSplit; // надеюсь я правильно понял про оператор и использовал только 1 (*)
 }
 
 console.log( String(`${numDegree}`.slice(0, 2)) );
