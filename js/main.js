7c668b50a2e4a1a542feff16ee139224f42e4d51
Alexandr <80934396+Vaso1911@users.noreply.github.com>
2023-08-19 04:46:05 +0300

    добавил сумму чисел

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 const num = 266219;
-const numSplit = String(num).split('').map(Number).reduce( (acc, value) => acc * value, 1);
+const numSplit = String(num).split('').map(Number);
+const numSplitMult = numSplit.reduce( (acc, value) => acc * value, 1);
+const numSplitSum = numSplit.reduce( (acc, value) => acc + value, 1);
 
+console.log(`${numSplitMult}`);
+console.log(`${numSplitSum}`);
 
-console.log(` ${numSplit}`);
-
-let numDegree = numSplit
+let numDegree = numSplitMult
 for (let i = 1; i < 3; i++) {
-  numDegree *= numSplit; // надеюсь я правильно понял про оператор и использовал только 1 (*)
+  numDegree *= numSplitMult; // надеюсь я правильно понял про оператор и использовал только 1 (*)
+}
+let numDegreeTwo = numSplitSum
+for (let i = 1; i < 3; i++) {
+  numDegreeTwo *= numSplitSum;
 }
 
 console.log( String(`${numDegree}`.slice(0, 2)) );
+console.log( String(`${numDegreeTwo}`.slice(0, 2)) );
