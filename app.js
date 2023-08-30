b1b057ea24bc1620c96b15238563a81451a927c7
Alexandr <80934396+Vaso1911@users.noreply.github.com>
2023-08-30 21:37:51 +0300

    hard-09

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
-const week = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
-const $list = document.querySelector('.list');
-let listHtml = '';
 
-const currentDate = new Date();
-const currentDayIndex = currentDate.getDay();
+const $dataOne = document.querySelector('.format-one');
+const $dataTwo = document.querySelector('.format-two');
 
-week.forEach((day, index) => {
-  let dayHtml = '<li class="list__item"><h2>';
-  
-  if (index === 0 || index === 6) {
-    dayHtml += '<span class="text-italic">';
-  }
-  
-  if (index === currentDayIndex) {
-    dayHtml += '<span class="text-bold">';
-  }
-  
-  dayHtml += day;
-  
-  if (index === 0 || index === 6 || index === currentDayIndex) {
-    dayHtml += '</span>';
-  }
-  
-  dayHtml += '</h2></li>';
-  
-  listHtml += dayHtml;
-});
+const formatTime = (num) => {
+  return num < 10 ? '0' + num : num;
+}
 
-$list.innerHTML = listHtml;
+const getDeclination = (num, a, b, c) => {
+
+  if (num == 1 || num % 10 == 1 && num != 11) {
+    return a;
+  } else if ((num >= 2 && num <= 4) || (num % 10 >= 2 && num % 10 <= 4 && (num < 10 || num > 20))) {
+    return b;
+  } else {
+    return c;
+  }
+}
+const getDeclinationHours = (hours) => {
+  return getDeclination(hours, 'час', 'часа', 'часов')
+}
+const getDeclinationMinutes = (minutes) => {
+  return getDeclination(minutes, 'минута', 'минуты', 'минут')
+}
+const getDeclinationSeconds = (seconds) => {
+  return getDeclination(seconds, 'секунда', 'секунды', 'секунд')
+}
+
+
+const getTime = () => {
+  const now = new Date();
+
+  const arrayWeek = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+  const months = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
+
+  const formatOne = `Сегодня ${arrayWeek[now.getDay()]}, ${now.getDate()} ${months[now.getMonth()]} ${now.getFullYear()} года, ${now.getHours()} ${getDeclinationHours(now.getHours())} ${now.getMinutes()} ${getDeclinationMinutes(now.getMinutes())} ${now.getSeconds()} ${getDeclinationSeconds(now.getSeconds())}`;
+  const formatTwo = `${formatTime(now.getDate())}.${formatTime(now.getMonth() + 1)}.${now.getFullYear()} - ${formatTime(now.getHours())}:${formatTime(now.getMinutes())}:${formatTime(now.getSeconds())}`;
+
+  $dataOne.innerHTML = formatOne;
+  $dataTwo.innerHTML = formatTwo;
+}
+
+getTime();
+setInterval(getTime, 1000);
